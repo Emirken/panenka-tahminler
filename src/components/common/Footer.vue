62a117220bfcca73aabc771dc1ebbf93f2a14b5e
Emircan <emircanadak@gmail.com>
2025-10-16 11:10:01 +0300

    footer da görünüm düzenlemeleri yapıldı

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="app-footer">
     <v-container>
       <v-row align="center">
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="4">
           <div class="d-flex align-center mb-4 mb-md-0 justify-center justify-md-start">
             <v-avatar size="32" color="primary" class="mr-3">
               <v-icon color="white" size="20">mdi-soccer</v-icon>
@@ -11,35 +11,21 @@
           </div>
         </v-col>
 
-        <v-col cols="12" md="6" class="text-center text-md-right">
-          <span class="text-body-2">© 2023 Panenka Tahminler. Tüm hakları saklıdır.</span>
+        <v-col cols="12" md="4" class="text-center">
+          <v-btn
+              icon
+              variant="text"
+              size="small"
+              color="white"
+              href="https://instagram.com"
+              target="_blank"
+          >
+            <v-icon>mdi-instagram</v-icon>
+          </v-btn>
         </v-col>
-      </v-row>
 
-      <v-row class="mt-4">
-        <v-col cols="12" class="text-center">
-          <v-btn
-              icon
-              variant="text"
-              size="small"
-              color="white"
-              href="https://facebook.com"
-              target="_blank"
-              class="mx-2"
-          >
-            <v-icon>mdi-facebook</v-icon>
-          </v-btn>
-          <v-btn
-              icon
-              variant="text"
-              size="small"
-              color="white"
-              href="https://twitter.com"
-              target="_blank"
-              class="mx-2"
-          >
-            <v-icon>mdi-twitter</v-icon>
-          </v-btn>
+        <v-col cols="12" md="4" class="text-center text-md-right">
+          <span class="text-body-2">© 2025 Panenka Tahminler. Tüm hakları saklıdır.</span>
         </v-col>
       </v-row>
     </v-container>
